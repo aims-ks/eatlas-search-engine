1acd7bf80d6889da9da89ebc02b7528344ea417b
gaellafond <g.lafond@aims.gov.au>
2024-04-15 18:06:08 +0800

    Run JUnit tests using the save version of Elastic Search as the application.

--- src/test/java/au/gov/aims/eatlas/searchengine/ToDeleteTemporaryTest.java
+++ src/test/java/au/gov/aims/eatlas/searchengine/ToDeleteTemporaryTest.java
@@ -12,14 +12,32 @@ import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * This test was created to experiment with the "Testcontainers" framework library.
  */
 @Testcontainers
 public class ToDeleteTemporaryTest {
+
+    public static String getElasticsearchVersion() {
+        // Load the Elastic Search version found in the test.properties file,
+        // after being substituted by Maven with the actual version number found in the POM.
+        try (InputStream input = ToDeleteTemporaryTest.class.getClassLoader()
+                .getResourceAsStream("test.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+            return prop.getProperty("elasticsearch.version");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Container
     private static final ElasticsearchContainer elasticsearchContainer =
-        new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:8.3.0")
+        new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:" + ToDeleteTemporaryTest.getElasticsearchVersion())
             // Restrict to single node (for testing purposes)
             .withEnv("discovery.type", "single-node")
             // Restrict memory usage
