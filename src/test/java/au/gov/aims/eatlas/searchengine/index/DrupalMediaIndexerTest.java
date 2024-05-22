4e890bcd2fdfb02e3e88e9eb32ab151e31fb90e0
Gael Lafond <g.lafond@aims.gov.au>
2024-05-22 12:13:17 +0800

    Add test for DrupalMediaIndexer

---
+++ src/test/java/au/gov/aims/eatlas/searchengine/index/DrupalMediaIndexerTest.java
@@ -0,0 +1,74 @@
+package au.gov.aims.eatlas.searchengine.index;
+
+import au.gov.aims.eatlas.searchengine.admin.rest.Messages;
+import au.gov.aims.eatlas.searchengine.client.SearchClient;
+import au.gov.aims.eatlas.searchengine.rest.Search;
+import au.gov.aims.eatlas.searchengine.search.IndexSummary;
+import au.gov.aims.eatlas.searchengine.search.SearchResults;
+import au.gov.aims.eatlas.searchengine.search.Summary;
+import co.elastic.clients.elasticsearch._types.HealthStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+public class DrupalMediaIndexerTest extends IndexerTestBase {
+
+    @Test
+    public void testIndexImages() throws IOException {
+        try (SearchClient client = this.createElasticsearchClient()) {
+            Assertions.assertEquals(HealthStatus.Green, client.getHealthStatus(), "The Elastic Search engine health status is not Green before starting the test.");
+
+            String index = "images";
+            Messages messages = Messages.getInstance(null);
+
+            client.createIndex(index);
+
+            // Find the indexer, defined in the config file
+            DrupalMediaIndexer drupalMediaIndexer =
+                    (DrupalMediaIndexer)this.getConfig().getIndexer(index);
+
+            String configPath = "drupalImageFiles";
+            URL configUrl = DrupalMediaIndexerTest.class.getClassLoader().getResource(configPath);
+            drupalMediaIndexer.setDrupalUrl(configUrl.toString());
+
+            drupalMediaIndexer.internalIndex(client, null, messages);
+
+            // Wait for ElasticSearch to finish its indexation
+            client.refresh(index);
+
+            SearchResults results = null;
+            try {
+                String q = ""; // Search for an empty string, to get all the blocks
+                Integer start = 0;
+                Integer hits = 50;
+                String wkt = null; // No geographic filtering
+                List<String> idx = List.of(index);
+
+                results = Search.paginationSearch(client, q, start, hits, wkt, idx, null, messages);
+
+                Summary searchSummary = results.getSummary();
+
+                // Check indexes in the summary.
+                Map<String, IndexSummary> indexSummaryMap = searchSummary.getIndexSummaries();
+                Assertions.assertEquals(1, indexSummaryMap.size(),
+                        "Wrong number if index summary.");
+                Assertions.assertTrue(indexSummaryMap.containsKey(index),
+                        String.format("Missing index from the search summary: %s", index));
+
+                IndexSummary layersIndexSummary = searchSummary.getIndexSummary(index);
+
+                Assertions.assertEquals(5, layersIndexSummary.getHits(),
+                        "Wrong number of search result in the index summary.");
+
+            } catch(Exception ex) {
+                Assertions.fail("Exception thrown while testing the Search API.", ex);
+            }
+
+            Assertions.assertEquals(HealthStatus.Green, client.getHealthStatus(), "The Elastic Search engine health status is not Green after the test.");
+        }
+    }
+}
